22b401ea22ec769df9bbb9dac30b53c22bd4d32e
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-04-24 18:48:27 -0300

    :tada: initial settings

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,25 @@
+import { globalStyles} from '@styles/global';
+import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
+
+globalStyles();
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
+}
